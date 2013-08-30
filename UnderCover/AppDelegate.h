833d6856c03510c987aea53e8fe99ea6b2ac6760
wanbin <wanbin89731@163.com>
2013-08-31 00:10:09 +0800

    firstcommit

---
+++ UnderCover/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UnderCover
+//
+//  Created by 万斌 on 13-8-20.
+//  Copyright (c) 2013年 万斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
